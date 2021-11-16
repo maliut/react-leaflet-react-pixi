eb793ffc1a1a0d80c2922a69662500901fcdff53
maliut <lqn619@163.com>
2021-11-16 23:57:14 +0800

    combine leaflet-pixi-overlay

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import {MapContainer, TileLayer, Marker} from 'react-leaflet'
+import {MapContainer, TileLayer, Marker, useMap} from 'react-leaflet'
+import 'leaflet-pixi-overlay' // Must be called before the 'leaflet' import
 import L from 'leaflet'
+import * as PIXI from 'pixi.js'
+import {useEffect} from 'react'
 
 const position = [51.505, -0.09]
 
@@ -11,8 +14,39 @@ function App() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <Marker position={position} icon={L.divIcon({html: '我是 div icon'})}/>
+      <PixiRoot />
     </MapContainer>
   )
+}
+
+const container = new PIXI.Container()
+const text = new PIXI.Text('我是 Pixi Text', { fill: 0xff1010 })
+container.addChild(text)
+
+const pixiOverlay = L.pixiOverlay((utils) => {
+  const container = utils.getContainer()
+  const renderer = utils.getRenderer()
+  const project = utils.latLngToLayerPoint
+  const scale = utils.getScale()
+
+  const coords = project([51.505, -0.08])
+  text.x = coords.x
+  text.y = coords.y
+  text.text = '我是 Pixi Text ' + Math.floor(Math.random() * 100)
+  text.scale.set(1 / scale)
+
+  renderer.render(container)
+}, container)
+
+function PixiRoot() {
+  // 获取 map 实例，map 来自 react-leaflet 的 MapContainer
+  const map = useMap()
+
+  useEffect(() => {
+    pixiOverlay.addTo(map)
+  }, [map])
+
+  return null
 }
 
 export default App
